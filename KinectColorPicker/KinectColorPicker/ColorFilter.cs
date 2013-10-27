e79b959a14b2097401818d2790d53a7fea5b95ec
jgblight <jgblight@gmail.com>
2013-10-27 13:29:44 -0400

    binary erosion to improve blob detection

--- KinectColorPicker/KinectColorPicker/ColorFilter.cs
+++ KinectColorPicker/KinectColorPicker/ColorFilter.cs
@@ -16,16 +16,18 @@ namespace KinectColorPicker
         private bool applyFilters;
         private YCbCr color;
         private YCbCrFiltering colorFilter;
-        private GrayscaleBT709 grayFilter;
+        private GrayscaleRMY grayFilter;
         private Threshold binaryFilter;
+        private BinaryErosion3x3 erosionFilter;
         private GrayscaleToRGB rgbFilter;
 
         public ColorFilter()
         {
             this.applyFilters = false;
             this.colorFilter = new YCbCrFiltering();
-            this.grayFilter = new GrayscaleBT709();
+            this.grayFilter = new GrayscaleRMY();
             this.binaryFilter = new Threshold(1);
+            this.erosionFilter = new BinaryErosion3x3();
             this.rgbFilter = new GrayscaleToRGB();
         }
 
@@ -35,7 +37,7 @@ namespace KinectColorPicker
             this.color = YCbCr.FromRGB(rgbcolor);
             this.colorFilter.Cb = new AForge.Range(this.color.Cb - 0.02f, this.color.Cb + 0.02f);
             this.colorFilter.Cr = new AForge.Range(this.color.Cr - 0.02f, this.color.Cr + 0.02f);
-            this.colorFilter.Y = new AForge.Range(this.color.Y - 0.1f, this.color.Y + 0.1f);
+            this.colorFilter.Y = new AForge.Range(this.color.Y - 0.05f, this.color.Y + 0.05f);
             this.applyFilters = true;
         }
 
@@ -53,6 +55,7 @@ namespace KinectColorPicker
                 this.colorFilter.ApplyInPlace(bmp);
                 Bitmap graybmp = this.grayFilter.Apply(bmp);
                 this.binaryFilter.ApplyInPlace(graybmp);
+                this.erosionFilter.ApplyInPlace(graybmp);
                 bmp = this.rgbFilter.Apply(graybmp).Clone(new Rectangle(0, 0, width, height), bmp.PixelFormat);
 
                 bmData = bmp.LockBits(new System.Drawing.Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, bmp.PixelFormat);
