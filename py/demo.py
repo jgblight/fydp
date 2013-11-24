1bd90c8f73c79a9fe82b70ca7e6576a95880f367
Sara G <smgreenb@uwaterloo.ca>
2013-11-24 13:13:09 -0500

    Temporal smoothing added to demo.py and calibration data in py/calibration/cal1 folder

--- py/demo.py
+++ py/demo.py
@@ -6,17 +6,27 @@ import sys
 import featureExtraction as fe
 from sklearn import svm
 from sklearn import cross_validation, grid_search
+from collections import deque
 import pprint
 import csv
 import pickle
 
 
+def median(mylist):
+    sorts = sorted(mylist)
+    length = len(sorts)
+    if not length % 2:
+        return (sorts[length / 2] + sorts[length / 2 - 1]) / 2.0
+    return sorts[length / 2]
+
 
 if __name__ == "__main__":
 
     modelfile = open(sys.argv[2])
+
     model = pickle.load(modelfile)
     labels = pickle.load(modelfile)
+    last10labels = deque([])
 
     with open(sys.argv[1]) as csvfile:
         reader = csv.reader(csvfile)
@@ -34,8 +44,19 @@ if __name__ == "__main__":
             features = fe.getFeatureVector(hull)
 
             imbgrclass = model.predict([features])
+            imbgrclass = imbgrclass[0]
+            
+            if len(last10labels) < 10:
+                sign = imbgrclass
+                last10labels.append(sign)
+            else:
+                sixth = last10labels.popleft()
+                last10labels.append(imbgrclass)
+                sign = median(last10labels)
 
-            print labels[imbgrclass[0]]
+            sign = int(round(sign))
+
+            print labels[sign]
 
         except KeyboardInterrupt:
             break
