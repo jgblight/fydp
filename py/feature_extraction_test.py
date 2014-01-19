7a82fad1a42624bdbf9b4d23b935c998d2a41968
Sara G <smgreenb@uwaterloo.ca>
2014-01-18 20:00:38 -0500

    Centroid temporal filtering

--- py/feature_extraction_test.py
+++ py/feature_extraction_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import featureExtraction as extract
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from collections import deque
 import cv
 import cv2
 import sys
@@ -10,6 +11,8 @@ import csv
 
 if __name__ == "__main__":
 
+    rightPast = deque([])
+    leftPast = deque([])
     with open(sys.argv[1]) as csvfile:
         reader = csv.reader(csvfile)
         low = [ float(x) for x in reader.next()]
@@ -50,8 +53,26 @@ if __name__ == "__main__":
             cv2.drawContours(imgray2,[greenhull],-1,(255,0,0),2)
             cv2.drawContours(imgray2,[redhull],-1,(255,0,0),2)
 
-            lefthand = extract.getCentroidPosition(imbgr,imdepth,red,blue)
-            righthand = extract.getCentroidPosition(imbgr,imdepth,green,blue)
+            leftcentroid = extract.getCentroidPosition(imbgr,imdepth,red,blue)
+            rightcentroid = extract.getCentroidPosition(imbgr,imdepth,green,blue)
+	    
+	    #begin centroid averaging filter
+	    if len(leftPast) < 10:
+		lefthand = leftcentroid
+		leftPast.append(leftcentroid)
+            else:
+		_ = leftPast.popleft()
+		leftPast.append(leftcentroid)
+		lefthand = tuple(map(np.mean, zip(*leftPast)))
+
+	    if len(rightPast) < 10:
+		righthand = rightcentroid
+		rightPast.append(rightcentroid)
+            else:
+		_ = rightPast.popleft()
+		rightPast.append(rightcentroid)
+		righthand = tuple(map(np.mean, zip(*rightPast)))
+	    #end centroid averaging filter
 
             ax.scatter(lefthand[0],lefthand[1],lefthand[2],c='r')
             ax.scatter(righthand[0],righthand[1],righthand[2],c='g')
