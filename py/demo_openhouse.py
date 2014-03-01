22c5b8c6307e0b75ac7b8f9566dc1087446aa33d
Jennifer Blight <jgblight@v1022-rn-1-96.campus-dynamic.uwaterloo.ca>
2014-02-28 22:35:22 -0500

    open house demo

--- py/demo_openhouse.py
+++ py/demo_openhouse.py
@@ -4,71 +4,86 @@ import cv2
 import os
 import sys
 import featureExtraction as fe
-from sklearn import svm
-from sklearn import cross_validation, grid_search
-from collections import deque
-import pprint
 import csv
 import pickle
+import time
+import calendar
 
 if __name__ == "__main__":
 
     modelfile = open(sys.argv[2])
 
-    model = pickle.load(modelfile)
+
     labels = pickle.load(modelfile)
+    models = pickle.load(modelfile)
+    f = fe.FeatureExtractor(sys.argv[1])
 
 
-    with open(sys.argv[1]) as csvfile:
-        reader = csv.reader(csvfile)
-        low = [ float(x) for x in reader.next()]
-        high = [ float(x) for x in reader.next()]
-	green = fe.colourFilter(tuple(low),tuple(high))
-	low = [ float(x) for x in reader.next()]
-        high = [ float(x) for x in reader.next()]
-        blue = fe.colourFilter(tuple(low),tuple(high))
-	low = [ float(x) for x in reader.next()]
-        high = [ float(x) for x in reader.next()]
-        red = fe.colourFilter(tuple(low),tuple(high))
+    for i in range(10):
+        rand_sign = np.random.randint(0,len(labels))
 
-    while 1:
-        try:
-            imbgr = fe.get_video()
-            greenhull = green.getColourHull(imbgr)
-	    bluehull = blue.getColourHull(imbgr)
-  	    redhull = red.getColourHull(imbgr)
-#haven't edited from hear on
-            if len(hull):
-                features = fe.getFeatureVector(hull,['central'])
+        while 1:
+            try:
+                imbgr = np.zeros((480,640,3))
+                cv2.putText(imbgr,labels[rand_sign],(5,50),cv2.FONT_HERSHEY_COMPLEX,2,(255,255,255),5)
 
-                imbgrclass = model.predict([features])
-                imbgrclass = imbgrclass[0]
-                
-                if len(running) < 10:
-                    sign = imbgrclass
-                    running.append(sign)
-                else:
-                    _ = running.popleft()
-                    running.append(imbgrclass)
-                    sign = median(running)
+                cv2.imshow("Demo",imbgr)
 
-                sign = int(round(sign))
-
-                letter = labels[sign]
-            else:
-                letter = ""
+            except KeyboardInterrupt:
+                break
+            if cv.WaitKey(10) == 32:
+                break
 
 
-            imgray = cv2.cvtColor(imbgr,cv.CV_BGR2GRAY)
+        f.setStartPoint()
 
-            cv2.drawContours(imgray,[hull],-1,(255,0,0),2)
-            cv2.putText(imgray,letter,(5,50),cv2.FONT_HERSHEY_COMPLEX,2,(255,255,255),5)
+        while 1:
+            try:
+                imbgr = np.array(fe.get_video())
+                imdepth = np.array(fe.get_depth())
 
-            cv2.imshow("Demo",imbgr)
+                cv2.imshow("Demo",imbgr)
+                f.addPoint(time.time(),imbgr,imdepth)
 
-            cv2.imshow("Hull",imgray)
+            except KeyboardInterrupt:
+                break
+            if cv.WaitKey(10) == 32:
+                break
 
-        except KeyboardInterrupt:
-            break
-        if cv.WaitKey(10) == 27:
-            break
+        obs = np.nan_to_num(f.getFeatures())
+
+        scores = []
+        for i in models:
+            scores.append(i.score(obs))
+
+        print scores
+        score_min = np.amin(scores)
+        score_max = np.amax(scores)
+
+        scores = (scores - score_min) / (score_max - score_min)
+
+        likelihood = scores[rand_sign]
+        print likelihood
+
+        if likelihood < 0.3 or likelihood == np.nan:
+            message = "Try Again!"
+        elif likelihood < 0.5:
+            message = "Weak"
+        elif likelihood < 0.8:
+            message = "Good Job"
+        else:
+            message = "Excellent!"
+
+
+
+        while 1:
+            try:
+                imbgr = np.array(fe.get_video())
+                cv2.putText(imbgr,message,(5,50),cv2.FONT_HERSHEY_COMPLEX,2,(255,255,255),5)
+
+                cv2.imshow("Demo",imbgr)
+
+            except KeyboardInterrupt:
+                break
+            if cv.WaitKey(10) == 32:
+                break 
